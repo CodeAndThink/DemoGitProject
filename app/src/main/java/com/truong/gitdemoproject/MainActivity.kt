7357d7e8000240bda778917f841a456afcac04cd
TruongNgo <87740125+CodeAndThink@users.noreply.github.com>
2024-05-28 13:57:20 +0700

    add more cmt in activity main

--- app/src/main/java/com/truong/gitdemoproject/MainActivity.kt
+++ app/src/main/java/com/truong/gitdemoproject/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
         //This cmt does not affect to anyone
+        //Add more comments
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
