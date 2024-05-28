569532cb1eb750b814fe1c837d73b0bdcfac52a9
TruongNgo <87740125+CodeAndThink@users.noreply.github.com>
2024-05-28 13:58:35 +0700

    delete all unrelated comments

--- app/src/main/java/com/truong/gitdemoproject/SecondFragment.kt
+++ app/src/main/java/com/truong/gitdemoproject/SecondFragment.kt
@@ -15,9 +15,6 @@ class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    //add cmt for main
     private val binding get() = _binding!!
 
     override fun onCreateView(
